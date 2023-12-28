63f91773be8d0f4e435ae3eac1899c8256998bdf
Eyver Emilio Vargas Medina <eyver.evm@gmail.com>
2023-12-28 11:34:18 -0400

    update : organize files
    - change config file path

---
+++ demo/01_frida.windows.py
@@ -0,0 +1,28 @@
+import frida
+
+def on_message(message, data):
+    print("[on_message] message:", message, "data:", data)
+
+try:
+    session = frida.attach("notepad++.exe")
+
+    script_code = """
+        rpc.exports.enumerateModules = function () {
+            return Process.enumerateModules();
+        };
+    """
+    script = session.create_script(script_code)
+    script.on("message", on_message)
+    script.load()
+
+    modules = script.exports_sync.enumerate_modules()
+    
+    for module in modules:
+        print(module["name"])
+
+except frida.ProcessNotFoundError:
+    print("Error: No se pudo encontrar el proceso 'notepad++.exe'. Asegúrate de que el proceso esté en ejecución.")
+except frida.PermissionDeniedError:
+    print("Error: Permiso denegado. Es posible que necesites privilegios de administrador para adjuntarte a este proceso.")
+except Exception as e:
+    print(f"Error inesperado: {e}")
---
+++ interactive/04.FA_list_methods.py
@@ -0,0 +1,51 @@
+import frida
+import sys
+import os
+import json
+
+def on_message(message, data):
+    if message['type'] == 'send':
+        print("[*] {0}".format(message['payload']))
+    else:
+        print(message)
+
+def load_config(config_file):
+    with open(config_file, 'r', encoding='utf-8') as file:
+        return json.load(file)
+
+def list_app_events(device_id, process_name, package_name, main_activity):
+    full_activity_name = package_name + "." + main_activity
+    jscode = """
+        Java.perform(function () {
+            var MainActivity = Java.use('""" + full_activity_name + """');
+            var methods = MainActivity.class.getDeclaredMethods();
+            methods.forEach(function (method) {
+                var methodName = method.getName();
+                send('Método encontrado: ' + methodName);
+                // Aquí podrías agregar enganches a métodos específicos
+            });
+        });
+    """
+    try:
+        print("> Adjuntando script a la aplicación...")
+        device = frida.get_device(device_id, timeout=2)
+        session = device.attach(process_name)
+        script = session.create_script(jscode)
+        script.on('message', on_message)
+        script.load()
+        print("> Script cargado. Monitoreando eventos...")
+        input("[*] Presiona enter para detener el script...")
+    except Exception as e:
+        print(f"> Error: {e}")
+
+def main():
+    os.system('cls' if os.name == 'nt' else 'clear')
+    config = load_config('../configMe.json')
+    device_id = config['device_id']
+    process_name = config['process_name']
+    package_name = config['package_name']
+    main_activity = config['main_activity']
+    list_app_events(device_id, process_name, package_name, main_activity)
+
+if __name__ == "__main__":
+    main()
