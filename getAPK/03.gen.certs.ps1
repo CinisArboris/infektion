d4f9573e016b19e94ca7795d6b9428f3d0c0490e
Eyver Emilio Vargas Medina <eyver.evm@gmail.com>
2024-01-03 11:20:25 -0400

    update : edit for 09

--- getAPK/03.gen.certs.ps1
+++ getAPK/03.gen.certs.ps1
@@ -21,13 +21,24 @@ if (-not (Test-Path -Path $keystoreDir)) {
     New-Item -ItemType Directory -Path $keystoreDir
 }
 
+# Comando para verificar si el alias ya existe en el keystore
+$checkAliasCommand = "keytool -list -keystore `"$keystorePath`" -alias `"$keystoreAlias`" -storepass `"$keystorePass`""
+
+# Comando para eliminar el alias si ya existe
+$deleteAliasCommand = "keytool -delete -alias `"$keystoreAlias`" -keystore `"$keystorePath`" -storepass `"$keystorePass`""
+
 # Comando para generar el keystore
 $keytoolCommand = "keytool -genkey -v -keystore `"$keystorePath`" -alias `"$keystoreAlias`" -keyalg `"$keyAlg`" -keysize $keySize -validity $validity -storepass `"$keystorePass`" -keypass `"$keystorePass`" -dname `"CN=Unknown, OU=Unknown, O=Unknown, L=Unknown, S=Unknown, C=Unknown`""
 
-# Ejecutar el comando
 try {
-    Invoke-Expression $keytoolCommand
+    # Suprimir salida detallada de los comandos keytool
+    $null = Invoke-Expression $checkAliasCommand 2>&1
+    $null = Invoke-Expression $deleteAliasCommand 2>&1
+    $null = Invoke-Expression $keytoolCommand 2>&1
+
     Write-Host "Keystore creado con éxito en: $keystorePath"
+    return "Success"
 } catch {
-    Write-Host "Error al crear el keystore."
+    Write-Host "Error durante la creación del keystore."
+    return "Error"
 }
